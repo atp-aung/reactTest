eab87694af4c8bc39fcb0de4bc1ef29c02a0387e
atp <atp.aung.5555@gmail.com>
2023-04-13 15:37:03 +0630

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
-
 const App = () => {
   const course = {
     id: 1,
@@ -27,33 +23,5 @@ const App = () => {
 
   return <Course course={course} />;
 };
-
-// const App = () => {
-//   const course = {
-//     name: "Half Stack application development",
-//     parts: [
-//       {
-//         name: "Fundamentals of React",
-//         exercises: 10,
-//       },
-//       {
-//         name: "Using props to pass data",
-//         exercises: 7,
-//       },
-//       {
-//         name: "State of a component",
-//         exercises: 14,
-//       },
-//     ],
-//   };
-//   return (
-//     <div>
-//       <Header course={course} />
-//       <Content course={course} />
-//       <br />
-//       <Total course={course} />
-//     </div>
-//   );
-// };
 
 export default App;
