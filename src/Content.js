3e95b28910039d92408e026fa516c927e48fb798
atp <atp.aung.5555@gmail.com>
2023-04-14 06:45:58 +0630

    a

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,12 @@
 const Content = ({ courses }) => {
-  return <div>{courses.part}</div>;
-  // const pa = courses.parts.map((p) => {
-  //   return (
-  //     <div key={p.id}>
-  //       {p.name}: {p.exercises}
-  //     </div>
-  //   );
-  // });
-  // return <div>{pa}</div>;
+  const pa = courses.parts.map((p) => {
+    return (
+      <div key={p.id}>
+        {p.name}: {p.exercises}
+      </div>
+    );
+  });
+  return <div>{pa}</div>;
 };
 
 export default Content;
