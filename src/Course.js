46e7c39abfd63eaefd40b5b6cb22e9111ded2933
atp <atp.aung.5555@gmail.com>
2023-04-15 07:18:43 +0630

    a

--- src/Course.js
+++ src/Course.js
@@ -1,25 +1,45 @@
 const Course = ({ courses }) => {
-  return (
-    <div>
-      {courses.map((c, id) => {
-        let a = 0;
-        return (
-          <div key={id}>
-            <h1>{c.name}</h1>
-            {c.parts.map((k, i) => {
-              a += k.exercises;
-              return (
-                <p key={i}>
-                  {k.name}: {k.exercises}
-                </p>
-              );
-            })}
-            <p>total:{a}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
+  const aa = courses.map((c, id) => {
+    let a = 0;
+    return (
+      <div key={id}>
+        <h1>{c.name}</h1>
+        {c.parts.map((k, i) => {
+          a += k.exercises;
+          return (
+            <p key={i}>
+              {k.name}: {k.exercises}
+            </p>
+          );
+        })}
+        <p>total:{a}</p>
+      </div>
+    );
+  });
+
+  return <div>{aa}</div>;
 };
+
+//   return (
+//     <div>
+//       {courses.map((c, id) => {
+//         let a = 0;
+//         return (
+//           <div key={id}>
+//             <h1>{c.name}</h1>
+//             {c.parts.map((k, i) => {
+//               a += k.exercises;
+//               return (
+//                 <p key={i}>
+//                   {k.name}: {k.exercises}
+//                 </p>
+//               );
+//             })}
+//             <p>total:{a}</p>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
 
 export default Course;
